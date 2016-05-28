efb06d2fdd1093ee1409f42a2b721a6008d24a35
Nick Golding <goldingn35@gmail.com>
2016-05-28 15:55:41 +1000

    package main doc

---
+++ R/pop.R
@@ -0,0 +1,13 @@
+#' @title pop: A Flexible Syntax for Population Dynamic Modelling
+#' @name pop
+#' @description Population dynamic models underpin a range of analyses and
+#'   applications in ecology and epidemiology. The various approaches for
+#'   fitting and analysing these models (MPMs, IPMs, ODEs, POMPs, PVA, with and
+#'   without metapopulation structure) are generally fitted using different
+#'   software, each with a different interface. This makes it difficult to
+#'   combine various modelling approaches and data types to solve a given
+#'   problem. pop aims to provide a flexible and easy to use common interface
+#'   for constructing population dynamic models and enabling to them to be
+#'   fitted and analysed in various ways.
+#' @docType package
+NULL
